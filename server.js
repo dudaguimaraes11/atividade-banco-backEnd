b329d249b20878297d29553db7c87191a096e03e
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-10-20 10:01:11 -0300

    Feat: Adiciona GetAll e GetById

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// Importar pacotes/bibliotecas
 import express from "express";
 import dotenv from "dotenv";
-//import bruxosRoutes from "./src/routes/bruxosRoutes.js";
+import bruxosRoutes from './src/routes/bruxosRoutes.js';
 
 // Criar aplicação com Express e configurar para aceitar JSON
 const app = express();
@@ -12,13 +13,14 @@ const serverPort = process.env.PORT || 3001;
 
 // Rota principal GET para "/"
 app.get("/", (req, res) => {
-    res.json({ message: "🪄 API dos Bruxos funcionando!" });
+    res.send("🚀 Servidor funcionando...");
 });
 
-// Rotas implementadas
-//app.use("/bruxos", bruxosRoutes);
+
+// Aqui vão todas suas Rotas
+app.use('/bruxos', bruxosRoutes);
 
 // Iniciar servidor escutando na porta definida
 app.listen(serverPort, () => {
-    console.log(`🪄 Servidor rodando em http://localhost:${serverPort}`);
+    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
 });
---
+++ src/routes/bruxosRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as BruxoController from './../controllers/bruxoControllers.js'
+
+const router = Router();
+
+// Rota GetAll em / 
+router.get("/", BruxoController.listarTodos);
+
+// Rota GetById
+router.get ("/:id", BruxoController.listarUm);
+
+export default router;
